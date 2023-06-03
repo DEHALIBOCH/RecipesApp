701a81ca1b4ad51b7a397baf740a43d74c8b7673
Marat <almabekov.marat@gmail.com>
2023-06-03 14:28:29 +0600

    Preparing for google authentication.

--- app/src/main/java/com/demoapp/recipesapp/AuthenticationActivity.java
+++ app/src/main/java/com/demoapp/recipesapp/AuthenticationActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.demoapp.recipesapp.databinding.ActivityAuthenticationBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthenticationActivity extends AppCompatActivity {
 
     private ActivityAuthenticationBinding binding;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
