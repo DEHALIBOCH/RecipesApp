1f9d1d5ef5c5a7eb05dee84cee0a61fec3975914
Marat <almabekov.marat@gmail.com>
2023-06-04 00:53:27 +0600

    Repository for firebase.

--- app/src/main/java/com/demoapp/recipesapp/domain/firebase/FirebaseRepository.java
+++ app/src/main/java/com/demoapp/recipesapp/domain/firebase/FirebaseRepository.java
@@ -5,10 +5,40 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class FirebaseRepository {
 
-    final static String dataBaseUrl= "https://recipesapp-3020b-default-rtdb.europe-west1.firebasedatabase.app/";
+    final static String dataBaseUrl = "https://recipesapp-3020b-default-rtdb.europe-west1.firebasedatabase.app/";
+    private final FirebaseDatabase DATABASE;
+    private final DatabaseReference USERS;
+    private static final FirebaseRepository INSTANCE = new FirebaseRepository();
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance(dataBaseUrl);
-    DatabaseReference users = database.getReference().child("Users");
+    private FirebaseRepository() {
+        DATABASE = FirebaseDatabase.getInstance(dataBaseUrl);
+        USERS = DATABASE.getReference().child("Users");
+    }
 
+    /**
+     * Метод возвращающий ссылку на ветку Users базы данных.
+     *
+     * @return Ссылка на ветку Users.
+     */
+    public DatabaseReference getUSERS() {
+        return USERS;
+    }
 
+    /**
+     * Метод возвращающий ссылку на базу данных.
+     *
+     * @return Ссылка на базу данных.
+     */
+    public FirebaseDatabase getDATABASE() {
+        return DATABASE;
+    }
+
+    /**
+     * Метод возвращающий синглтон FirebaseRepository, служащий обэектом доступа к базе данных.
+     *
+     * @return Возвращает экземпляр FirebaseRepository
+     */
+    public static FirebaseRepository getInstance() {
+        return INSTANCE;
+    }
 }
