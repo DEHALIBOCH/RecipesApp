a7918f0c840851ea66a02d83b057e1bb1a468201
Marat <almabekov.marat@gmail.com>
2023-06-06 23:34:01 +0600

    RecyclerView for TrendingRecipes in HomeFragment

---
+++ app/src/main/java/com/demoapp/recipesapp/fragments/recyclerutils/homefragment/TrendingNowRecipes.java
@@ -0,0 +1,50 @@
+package com.demoapp.recipesapp.fragments.recyclerutils.homefragment;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.demoapp.recipesapp.data.Recipe;
+import com.demoapp.recipesapp.databinding.TrendingNowRecipesRcItemBinding;
+
+import java.util.ArrayList;
+
+public class TrendingNowRecipes extends RecyclerView.Adapter<TrendingNowRecipes.ViewHolder> {
+
+    private ArrayList<Recipe> recipes;
+
+    public TrendingNowRecipes(ArrayList<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        TrendingNowRecipesRcItemBinding binding = TrendingNowRecipesRcItemBinding
+                .inflate(LayoutInflater.from(parent.getContext()), parent, false);
+
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // TODO дизайн для ресайклера
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TrendingNowRecipesRcItemBinding binding;
+
+        public ViewHolder(TrendingNowRecipesRcItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
