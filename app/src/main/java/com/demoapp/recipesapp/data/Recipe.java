5ad1794a51b77e788b4bd0a1ed4f19d807860604
Marat <almabekov.marat@gmail.com>
2023-06-07 01:36:16 +0600

    Class for storing data about recipe

--- app/src/main/java/com/demoapp/recipesapp/data/Recipe.java
+++ app/src/main/java/com/demoapp/recipesapp/data/Recipe.java
@@ -1,4 +1,8 @@
 package com.demoapp.recipesapp.data;
 
+/**
+ * Класс предназначенный для хранения основной информации о рецепте: автор, состав, рецепт, фото и т.д.
+ */
 public class Recipe {
+    // TODO
 }
