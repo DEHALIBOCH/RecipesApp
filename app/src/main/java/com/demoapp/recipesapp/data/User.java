bbebf073b48b129ba2edb3a5fdb1d9fbb93942c7
Marat <almabekov.marat@gmail.com>
2023-06-07 01:40:08 +0600

    User additional properties to store information about following and followers and class to provide relationship between users.

--- app/src/main/java/com/demoapp/recipesapp/data/User.java
+++ app/src/main/java/com/demoapp/recipesapp/data/User.java
@@ -1,12 +1,19 @@
 package com.demoapp.recipesapp.data;
 
+import java.util.ArrayList;
+
 public class User {
     private String name;
     private String lastname;
     private int age;
     private String email;
     private String password;
-    private String tokenUID;
+    private String tokenUID; // Уникальный идентификатор, используется для доступа к пользователь в бд
+    private String location;
+    private String bio;     // Краткое описание профиля, как в инстаграмме
+
+    private final ArrayList<String> followers = new ArrayList<>();    // Id подписчики
+    private final ArrayList<String> following = new ArrayList<>();    // Id подписок
 
     public User(String email, String tokenUID) {
         this.email = email;
@@ -57,4 +64,45 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public void addFollower(User user) {
+        followers.add(user.getTokenUID());
+    }
+
+    public void addFollowing(User user) {
+        followers.add(user.getTokenUID());
+    }
+
+    public void removeFollower(User user) {
+        followers.remove(user.getTokenUID());
+    }
+
+    public void removeFollowing(User user) {
+        followers.remove(user.getTokenUID());
+    }
+
+    public ArrayList<String> getFollowers() {
+        return followers;
+    }
+
+    public ArrayList<String> getFollowing() {
+        return following;
+    }
+
 }
